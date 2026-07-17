a34fb3402d8e7b0cf23d39565cf051380ae33d09
Anfal Eid <2seok003@gmail.com>
2026-07-17 08:43:34 +0300

    Add files via upload

---
+++ main/main/login_system.py
@@ -0,0 +1,17 @@
+def login_system_Home(Uname):
+    print('========================')
+    print('Login is accepted')
+    print(   'WELCOME',Uname )
+    print('========================')
+
+def main_menu():
+    print('====MENU====')
+    print('1.Show My profile')
+    print('2.change passWord')
+    print('3.Login out')
+    print('4.About ')
+
+def about():
+    print('Login system V1')
+    print('Created by Anfal')
+    print('Language : python')
