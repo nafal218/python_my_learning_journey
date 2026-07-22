1cb61e476b5609730030d7fbf1c39f0aa946ff52
Anfal Eid <2seok003@gmail.com>
2026-07-22 04:15:53 +0300

    Phone_book V2 .py

--- Phone book/Phone book/Phone_book.py
+++ Phone book/Phone book/Phone_book.py
@@ -1,38 +1,51 @@
 print('----Phone book----')
 
 users_info = [
-{ 'username': 'Amal','PhoneNumber':'1111111111'},    
-{ 'username': 'Mohammed','PhoneNumber':'2222222222'},    
-{ 'username': 'khadijah','PhoneNumber':'3333333333'}, 
-{ 'username': 'Adbullah','PhoneNumber':'4444444444'},    
-{ 'username': 'Rawan','PhoneNumber':'5555555555'},    
-{ 'username': 'Faisal','PhoneNumber':'6666666666'},    
-{ 'username': 'layla','PhoneNumber':'7777777777'}    
+{ 'username': 'Amal','PhoneNumber': '1111111111'},    
+{ 'username': 'Mohammed','PhoneNumber': '2222222222'},    
+{ 'username': 'khadijah','PhoneNumber': '3333333333'}, 
+{ 'username': 'Adbullah','PhoneNumber': '4444444444'},    
+{ 'username': 'Rawan','PhoneNumber': '5555555555'},    
+{ 'username': 'Faisal','PhoneNumber': '6666666666'},    
+{ 'username': 'layla','PhoneNumber': '7777777777'}    
     ]
 
-Ph_Number =input('Enter your phone Number : ')
-
-for i in users_info:
-    if Ph_Number == i['PhoneNumber']:
-     print('This is you name',i['username'], 'And this is your Phone number' ,i['PhoneNumber'])
-
-def Search_for_phone_number(x): 
-  for i in users_info:
-      if x == i['PhoneNumber']:
-         return True
-      
-  return False
-
-if Search_for_phone_number(Ph_Number) == False:
-    print('This number doesnt exist!')
-
-    answer = input('Do you wanna save this number (yes/no)?').lower()
-    if answer =='yes':
-        newPhonenum =input('Enter your new number : ')
-        newName =input('Enter your new name : ')
-
-        newUser = {'username':newName ,'PhoneNumber':newPhonenum }
-        users_info.append(newUser)
+#+++++++++++++++++++++++++++++++++++++++++++++++
+# يتحقق من الرقم 
+def Check_Ph_number(Ph_Number):
+    if len(Ph_Number) == 10 and Ph_Number.isdigit():
+        return True
     else:
-      print('GOOOD BYE <3')
+       print('It must be 10 numbers and without special Characters or letters ')
+       return False
+         
+#+++++++++++++++++++++++++++++++++++++++++++++++
+#  داله تتحقق من الرقم والاسم وتطبعه وفي نفس الوقت ترجع قيمعه البحث
+def Search_for_phone_number(Ph_Number): 
+    for i in users_info:
+        if Ph_Number == i['PhoneNumber']:
+         print(f"This is you name{i['username'] }And this is your Phone number{i['PhoneNumber']}")
+        
+         return True
+    return False
+#+++++++++++++++++++++++++++++++++++++++++++++++
 
+while True:
+   Ph_Number = input('Enter your phone Number : ')
+
+   if  Check_Ph_number(Ph_Number) == True:
+    if not  Search_for_phone_number(Ph_Number): 
+      print('This number doesnt exist!')
+
+      answer = input('Do you wanna save this number (yes/no)?').lower()
+      if answer =='yes':
+         newPhonenum = Ph_Number
+         newName = input('Enter your new name : ')
+         newUser = {'username':newName ,'PhoneNumber':newPhonenum }
+         users_info.append(newUser)
+      else:
+        print('GOOOD BYE <3')
+        
+   else:
+     continue
+     
